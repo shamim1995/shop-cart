281293f5c2f594013b637a40233f92c9d630e4dc
Hamidul Islam shamim <mdhamidul2714@gmail.com>
2021-08-29 01:18:23 +0600

    all complete

--- app.js
+++ app.js
@@ -1,28 +1,76 @@
-let increasingBtn = document.querySelector("#increasing");
-increasingBtn.addEventListener('click',function(){
-    let iphoneNumber= document.querySelector("#iphoneNumber");
-    let quantityNumber= iphoneNumber.value;
-    let iphoneIntNumber = parseInt(quantityNumber);
-    let addNumber = iphoneIntNumber + 1;
-    iphoneNumber.value =addNumber;
-    phoneAmount=addNumber*1200;
-    document.querySelector("#phoneAmount").innerText=phoneAmount;
 
-});
+function calculateShipping(product, isIncrease) {
+    let phoneNumber = document.querySelector(product+'-number');
+    let phoneIntNumber = parseInt(phoneNumber.value);
+    let addNumber = phoneIntNumber;
 
-document.querySelector("#dcreasing").addEventListener("click",function(){
-    let iphoneNumber= document.querySelector("#iphoneNumber");
-    let quantityNumber =iphoneNumber.value;
-    let iphoneIntNumber= parseInt(quantityNumber);
-    let minusNumber=quantityNumber-1;
-    iphoneNumber.value=minusNumber;
-    let phoneAmount = minusNumber*1200;
-   document.querySelector("#phoneAmount").innerText = phoneAmount;
+    if (isIncrease === true) {
+        addNumber = phoneIntNumber + 1;
+    } else if (isIncrease === false && addNumber > 0) {
+        addNumber = phoneIntNumber - 1;
+    }
+     phoneNumber.value = addNumber;
+     let phoneAmount = addNumber * 1200;
+
+    if(product==="#phone"){
+        phoneAmount=addNumber*1200;
+
+    }else if(product==="#cover"){
+    phoneAmount = addNumber * 59;
+    }
     
+    document.querySelector(product+"-price").innerText = phoneAmount;
+   
+    shopTotal();
+    
+};
 
-})
+function shopTotal() {
+    let phonePrice= document.querySelector("#phone-number");
+    let addPhonePrice=parseInt(phonePrice.value);
+    let coverPrice= document.querySelector("#cover-number");
+    let addCoverPrice=parseInt(coverPrice.value);
+    let totalAmount= addPhonePrice *1200+ addCoverPrice*59;
+    let taxAmount= totalAmount*0.1
+    let result = taxAmount+totalAmount;
+
+    document.querySelector('#sub-total').innerText=totalAmount;
+    document.querySelector('#tax').innerText = Math.round(taxAmount);
+    document.querySelector('#shop-total').innerText =Math.round(result);
+
+};
 
 
+// let increasingBtn = document.querySelector("#increasing");
+// increasingBtn.addEventListener('click',function(){
+//     calculateShipping(true);   
+// });
+
+// document.querySelector("#dcreasing").addEventListener("click", function () {
+//     calculateShipping(false);
+// });
+
+// function calculateShipping(isIncrease) {
+//      let iphoneNumber = document.querySelector("#iphoneNumber");
+//      let quantityNumber = iphoneNumber.value;
+//      let iphoneIntNumber = parseInt(quantityNumber);
+//      let addNumber = iphoneIntNumber;
+     
+//     if(isIncrease===true){
+//         addNumber = iphoneIntNumber + 1;
+//     } else if (isIncrease === false && addNumber > 0) {
+//        addNumber = iphoneIntNumber - 1;
+//     }
+//      iphoneNumber.value = addNumber;
+//      let phoneAmount = addNumber * 1200;
+//      document.querySelector("#phoneAmount").innerText = phoneAmount;
+// }
+
+
+
+
+
+//shipping close handler
 
 let closeBtn= document.querySelector("#closeBtn");
 closeBtn.addEventListener("click",function(){
